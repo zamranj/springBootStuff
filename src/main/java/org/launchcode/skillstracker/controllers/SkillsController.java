44f39cb49a5e444c8d3f33971125e92f175c47af
Zamran J <family_zj@yahoo.com>
2021-10-03 22:26:32 -0500

    Studio 8

---
+++ src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
@@ -0,0 +1,74 @@
+package org.launchcode.skillstracker.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class SkillsController {
+
+   @GetMapping()
+   @ResponseBody
+   public String listLanguages(){
+
+       return "<h1> Skills Tracker</h1>"+
+               "<h2>We have a few skills we would like to learn.Here is the list!</h2>"+
+               "<ol>"+
+               "<li>Java</li>"+
+               "<li>JavaScript</li>"+
+               "<li>Python</li>"+
+               "</ol>";
+   }
+
+   @GetMapping("form")
+    @ResponseBody
+    public String getForm(){
+       return "<form method='post'>"
+               +"<h3>Name:</h3>"
+               +"<input name ='name'/>"
+               +"<h3>My first favorite language</h3>"
+               +"<select name= 'firstChoice'>"
+                   +"<option value='Java'>Java</option>"
+                   +"<option value='JavaScript'>JavaScript</option>"
+                   +"<option value='Python'>Python</option>"
+               +"</select>"
+               +"<h3>My second favorite language</h3>"
+               +"<select name= 'secondChoice'>"
+                   +"<option value='Java'>Java</option>"
+                   +"<option value='JavaScript'>JavaScript</option>"
+                   +"<option value='Python'>Python</option>"
+               +"</select>"
+               +"<h3>My third favorite language</h3>"
+               +"<select name= 'thirdChoice'>"
+                   +"<option value='Java'>Java</option>"
+                   +"<option value='JavaScript'>JavaScript</option>"
+                   +"<option value='Python'>Python</option>"
+               +"</select>"
+               +"<br>"
+               +"<br>"
+               +"<input type='submit' value='Submit'/>"
+               +"</form>"
+               ;
+   }
+   @PostMapping("form")
+
+    public String submitForm(@RequestParam String name, @RequestParam String firstChoice,
+                             @RequestParam String secondChoice,@RequestParam String thirdChoice){
+        return "redirect:/results/" + name+"/"+firstChoice+"/"+secondChoice+"/"+thirdChoice;
+
+   }
+
+   @GetMapping("results/{name}/{firstChoice}/{secondChoice}/{thirdChoice}")
+    @ResponseBody
+    public String displayResults(@PathVariable String name, @PathVariable String firstChoice, @PathVariable String secondChoice,
+                                @PathVariable String thirdChoice){
+
+       return "<h2>"+name+"</h2>"
+               +"<ol>"
+               +"<li>"+firstChoice + "</li>"
+               +"<li>"+secondChoice + "</li>"
+               +"<li>"+thirdChoice + "</li>"
+               +"</ol>";
+   }
+
+
+}
